98b46852f37f531f16d49f0d3ae14c10c2aae93e
Edwin Sam <edwin@edwins-mbp-3.lan>
2022-02-25 17:37:36 -0800

    RMF-2-1 Add homepage components and get locations request
    RMF-2-1 Add homepage components and get locations request

---
+++ src/Components/RMHeader/rmheader.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../RMHeader/rmheader.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header-container">
+        <div className="name-container">
+          <h1 className="name">Rick and Morty</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
